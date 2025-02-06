2e2ced4962cb25c9c206862c666c1bc4c9a094a4
mbelenmorris <belen.morris@gmail.com>
2025-02-06 19:42:13 -0300

    .

---
+++ Script inicial.R
@@ -0,0 +1,7 @@
+library(eph)
+prueba_get_microdata <- get_microdata(year = 2019, trimester = 3, type = "individual")
+prueba_get_microdata2 <- get_microdata(year = 2019:2023, trimester = 3, type = "individual")
+summary(prueba_get_microdata2)
+min(prueba_get_microdata2$ANO4)
+max(prueba_get_microdata2$ANO4)
+library(openxlsx)
